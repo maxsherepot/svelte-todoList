5015a91f542051f2cc1ba51d48af7dc62225661c
Max <maksym@smartrr.com>
2023-11-16 13:14:27 +0200

    redundant removed, bootstrap added

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,20 @@
 <script>
 </script>
 
-<h1>hello</h1>
+<div class="container">
+    <span>Add new task</span>
+    <div class="input-group mb-3">
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Recipient's username"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+        />
+        <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2">Button</button
+        >
+    </div>
+</div>
